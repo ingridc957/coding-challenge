77348c01f1f43e0746058d872ee6182eeac14a47
IngridC <ingridc957@gmail.com>
2018-10-22 11:01:18 -0500

    para corregir el manejo de objetos

---
+++ Drawing_Tool.rb
@@ -0,0 +1,9 @@
+class DrawingTool
+# out = ''
+  # while out == ''
+  while 1 == 1
+    puts 'Plese enter your command'
+    command = gets.chomp
+    ImplementedCommands.new.interpreter(command)
+  end
+end
---
+++ console_paint_main.rb
@@ -0,0 +1,5 @@
+puts 'Welcome to a little Paint in our console'
+
+load 'Implemented_Commands.rb'
+load 'Validation_Command.rb'
+load 'Drawing_Tool.rb'
